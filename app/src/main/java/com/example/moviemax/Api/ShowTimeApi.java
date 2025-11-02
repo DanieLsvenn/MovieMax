4a730d41ad3487b7e34edd6912067623e482d38c
khoaNamK3 <your_email@gmail.com>
2025-11-02 13:06:57 +0700

    fix DTO

--- app/src/main/java/com/example/moviemax/Api/ShowTimeApi.java
+++ app/src/main/java/com/example/moviemax/Api/ShowTimeApi.java
@@ -1,4 +1,14 @@
 package com.example.moviemax.Api;
 
+import com.example.moviemax.Model.ShowTimeDto.ShowTimeResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
 public interface ShowTimeApi {
+    @GET("showtimes")
+    Call<List<ShowTimeResponse>> getShowTimes();
 }
--- app/src/main/java/com/example/moviemax/Model/SeatDto/SeatRequest.java
+++ app/src/main/java/com/example/moviemax/Model/SeatDto/SeatRequest.java
@@ -1,4 +1,21 @@
 package com.example.moviemax.Model.SeatDto;
 
 public class SeatRequest {
+
+    private String status;
+
+    public SeatRequest(){}
+
+    public SeatRequest(String status){
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
